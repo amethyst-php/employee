053ce922abf5630c218f719376acdcaff07ed85b
railken <railken@protonmail.ch>
2019-03-19 17:12:59 +0100

    update relations

--- src/Models/Employee.php
+++ src/Models/Employee.php
@@ -3,6 +3,8 @@
 namespace Railken\Amethyst\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Railken\Amethyst\Common\ConfigurableModel;
 use Railken\Lem\Contracts\EntityContract;
@@ -25,7 +27,7 @@ class Employee extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function legal_entity()
+    public function legal_entity(): BelongsTo
     {
         return $this->belongsTo(LegalEntity::class);
     }
@@ -33,7 +35,7 @@ class Employee extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function office()
+    public function office(): BelongsTo
     {
         return $this->belongsTo(Office::class);
     }

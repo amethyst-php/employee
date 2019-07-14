13ba7ee6cbb1ddf959e6117e5825a0b6514af104
railken <railken@protonmail.ch>
2019-07-14 13:08:36 +0200

    fix style

--- src/Managers/EmployeeManager.php
+++ src/Managers/EmployeeManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Employee newEntity()
- * @method \Amethyst\Schemas\EmployeeSchema getSchema()
+ * @method \Amethyst\Models\Employee                 newEntity()
+ * @method \Amethyst\Schemas\EmployeeSchema          getSchema()
  * @method \Amethyst\Repositories\EmployeeRepository getRepository()
- * @method \Amethyst\Serializers\EmployeeSerializer getSerializer()
- * @method \Amethyst\Validators\EmployeeValidator getValidator()
- * @method \Amethyst\Authorizers\EmployeeAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\EmployeeSerializer  getSerializer()
+ * @method \Amethyst\Validators\EmployeeValidator    getValidator()
+ * @method \Amethyst\Authorizers\EmployeeAuthorizer  getAuthorizer()
  */
 class EmployeeManager extends Manager
 {

29a5ee74d14f75341b592c89776d065782ffde90
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/EmployeeManager.php
+++ src/Managers/EmployeeManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Employee newEntity()
+ * @method \Amethyst\Schemas\EmployeeSchema getSchema()
+ * @method \Amethyst\Repositories\EmployeeRepository getRepository()
+ * @method \Amethyst\Serializers\EmployeeSerializer getSerializer()
+ * @method \Amethyst\Validators\EmployeeValidator getValidator()
+ * @method \Amethyst\Authorizers\EmployeeAuthorizer getAuthorizer()
+ */
 class EmployeeManager extends Manager
 {
     use ConfigurableManager;
